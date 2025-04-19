b49aee29c599209b5dd6269af7eb5a319ed57a9f
MemeFrug <knightmax22@gmail.com>
2025-04-19 13:53:03 +0930

    Worked on example

--- Examples/RawWebGPU/main.js
+++ Examples/RawWebGPU/main.js
@@ -1,3 +1,10 @@
 console.log("main.js loaded");
 
-// First I need to
+// First I need to access WebGPU's API
+const entry = navigator.gpu;
+if (!entry) {
+    throw new Error("WebGPU does not exist on this browser.");
+}
+
+// I need to get an adaptor 
+let adaptor = null;
